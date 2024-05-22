207a8287f8e63b711b05b122a413629e64911a98
Bruno Miguel Ataide Fortes <52974677+Bruno081002@users.noreply.github.com>
2024-05-22 11:47:18 +0100

    merging into Jose

---
+++ proj/src/moves.c
@@ -0,0 +1,10 @@
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+#include <stdio.h>
+#include "moves.h"  
+
+
+void PawnMove(){
+  printf("PawnMove\n");
+}
---
+++ proj/src/moves.h
@@ -0,0 +1,19 @@
+#ifndef __MOVES_H
+#define __MOVES_H
+
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+#include <stdio.h>
+
+
+void PawnMove();
+void RookMove();
+void KnightMove();
+void BishopMove();
+void QueenMove();
+void KingMove();
+void MovePiece();
+
+
+#endif
